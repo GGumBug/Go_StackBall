a0aea784af411f9a389c464515f446f79ff4d9a4
GGumBug <alsrl7538@gmail.com>
2023-03-24 11:12:39 +0900

    Level 추가

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
+    [Header("Common")]
+    [SerializeField]
+    private TextMeshProUGUI currentLevel;
+    [SerializeField]
+    private TextMeshProUGUI nextLevel;
+
     [Header("Main")]
     [SerializeField]
-    private GameObject mainPanel;
+    private GameObject      mainPanel;
+
+    private void Awake()
+    {
+        currentLevel.text   = (PlayerPrefs.GetInt("LEVEL")+1).ToString();
+        nextLevel.text      = (PlayerPrefs.GetInt("LEVEL")+2).ToString();
+    }
 
     public void GameStart()
     {
